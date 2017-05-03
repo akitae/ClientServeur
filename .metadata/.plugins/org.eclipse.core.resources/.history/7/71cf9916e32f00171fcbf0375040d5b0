088523b96edd763221185f49b72f6500c5549508
noemieleitao <noemie.leitao@hotmail.fr>
2017-05-03 12:19:59 +0200

    test serv

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7/71cf9916e32f00171fcbf0375040d5b0
@@ -0,0 +1,8 @@
+package cs;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface StockRemote {
+
+}
---
+++ Client/ejbModule/cs/Stock.java
@@ -0,0 +1,20 @@
+package cs;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Stock
+ */
+@Stateless
+@LocalBean
+public class Stock implements StockRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public Stock() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
