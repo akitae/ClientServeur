f34d49440ee39c82c014b935155452d4437f0fdc
noemieleitao <noemie.leitao@hotmail.fr>
2017-05-31 09:39:24 +0200

    remove useless file

---
+++ Serveur/src/Stock.java
@@ -0,0 +1,50 @@
+import java.io.Serializable;
+
+
+public class Stock implements Serializable{
+	 private int id;
+	 private String reference;
+	 private String familleComposant;
+	 private float prix;
+	 private int nombre;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFamilleComposant() {
+		return familleComposant;
+	}
+
+	public void setFamilleComposant(String familleComposant) {
+		this.familleComposant = familleComposant;
+	}
+
+	public float getPrix() {
+		return prix;
+	}
+
+	public void setPrix(float prix) {
+		this.prix = prix;
+	}
+
+	public int getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(int nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getReference() {
+		return reference;
+	}
+
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+}
